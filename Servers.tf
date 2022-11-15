e64f339c199223dfa5fda9bb27cfa39b04076c7f
Narek <zzz@zzz.zzz>
2022-11-15 16:57:25 +0400

    modules

--- Servers.tf
+++ Servers.tf
@@ -16,12 +16,37 @@ module "asg" {
   min_size                  = 2
   max_size                  = 4
   desired_capacity          = 2
-  wait_for_capacity_timeout = 0
   health_check_type         = "EC2"
   vpc_zone_identifier       = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
   target_group_arns = module.alb.target_group_arns
 
-  
+   initial_lifecycle_hooks = [
+    {
+      name                  = "ExampleStartupLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 60
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                  = "ExampleTerminationLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 180
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+  ]
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
   # Launch template
   launch_template_name        = "Development-asg"
   launch_template_description = "Ubuntu_Nginx"
